d6603347a961b203370ca55f557f85d9902ede2a
ccaups <ccaups@gmail.com>
2024-09-23 12:23:05 +0300

    SpaceshipIndex and etc

---
+++ ShopTARge23/Controllers/SpaceshipsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge23.Data;
+using ShopTARge23.Models.Spaceships;
+
+namespace ShopTARge23.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly ShopTARge23Context _context;
+
+        public SpaceshipsController
+            (
+            ShopTARge23Context context
+            )
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipsIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Typename = x.Typename,
+                    BuiltDate = x.BuiltDate,
+                    Crew = x.Crew,
+                });
+
+            return View(result);
+        }
+    }
+}
--- ShopTARge23/Program.cs
+++ ShopTARge23/Program.cs
@@ -1,9 +1,17 @@
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ShopTARge23Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
