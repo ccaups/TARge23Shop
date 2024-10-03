3bb9da1a50d9d9265cf292d318773c9bceaeecde
ccaups <ccaups@gmail.com>
2024-10-03 20:09:16 +0300

    Added KindergartensIndexVM, Index and Controller Index

---
+++ ShopTARge23/Controllers/KindergartensController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge23.Core.ServiceInterface;
+using ShopTARge23.Data;
+using ShopTARge23.Models.Kindergartens;
+using ShopTARge23.Models.Spaceships;
+
+namespace ShopTARge23.Controllers
+{
+    public class KindergartensController : Controller
+    {
+        private readonly ShopTARge23Context _context;
+        private readonly IKindergartensServices _kindergartensServices;
+
+        public KindergartensController
+            (
+                ShopTARge23Context context,
+                IKindergartensServices kindergartensServices
+            )
+        {
+            _context = context;
+            _kindergartensServices = kindergartensServices;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Kindergartens
+                .Select(x => new KindergartensIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergartenName = x.KindergartenName,
+                    Teacher = x.Teacher
+                });
+
+            return View(result);
+        }
+    }
+}
