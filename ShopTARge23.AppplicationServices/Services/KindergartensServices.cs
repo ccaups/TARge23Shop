d723e8937cce3f52412741bb139c219b16c91b91
ccaups <ccaups@gmail.com>
2024-10-03 19:11:27 +0300

    Revert "KindergartensServices Update added."
    This reverts commit a5779048f254da2717d3da0f7800124d0112eb1f.

--- ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
+++ ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ShopTARge23.Core.Domain;
-using ShopTARge23.Core.Dto;
-using ShopTARge23.Core.ServiceInterface;
-using ShopTARge23.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,42 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShopTARge23.ApplicationServices.Services
 {
-    public class KindergartensServices : IKindergartensServices
+    class KindergartensServices
     {
-        private readonly ShopTARge23Context _context;
-
-        public KindergartensServices 
-            (
-            ShopTARge23Context context
-            ) 
-        {
-            _context = context;
-        }
-
-        public async Task<Kindergarten> Create(KindergartenDto dto)
-        {
-            Kindergarten kindergarten = new Kindergarten();
-
-            kindergarten.Id = Guid.NewGuid();
-            kindergarten.GroupName = dto.GroupName;
-            kindergarten.ChildrenCount = dto.ChildrenCount;
-            kindergarten.KindergartenName = dto.KindergartenName;
-            kindergarten.Teacher = dto.Teacher;
-            kindergarten.CreatedAt = DateTime.Now;
-            kindergarten.UpdatedAt = DateTime.Now;
-
-            await _context.Kindergartens.AddAsync(kindergarten);
-            await _context.SaveChangesAsync();
-
-            return kindergarten;
-        }
-
-        public async Task<Kindergarten> DetailAsync(Guid id)
-        {
-            var result = await _context.Kindergartens
-                .FirstOrDefaultAsync(x => x.Id == id);
-
-            return result;
-        }
     }
 }
