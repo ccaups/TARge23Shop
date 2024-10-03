a5779048f254da2717d3da0f7800124d0112eb1f
ccaups <ccaups@gmail.com>
2024-10-03 19:10:21 +0300

    KindergartensServices Update added.

--- ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
+++ ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.Dto;
+using ShopTARge23.Core.ServiceInterface;
+using ShopTARge23.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,42 @@ using System.Threading.Tasks;
 
 namespace ShopTARge23.ApplicationServices.Services
 {
-    class KindergartensServices
+    public class KindergartensServices : IKindergartensServices
     {
+        private readonly ShopTARge23Context _context;
+
+        public KindergartensServices 
+            (
+            ShopTARge23Context context
+            ) 
+        {
+            _context = context;
+        }
+
+        public async Task<Kindergarten> Create(KindergartenDto dto)
+        {
+            Kindergarten kindergarten = new Kindergarten();
+
+            kindergarten.Id = Guid.NewGuid();
+            kindergarten.GroupName = dto.GroupName;
+            kindergarten.ChildrenCount = dto.ChildrenCount;
+            kindergarten.KindergartenName = dto.KindergartenName;
+            kindergarten.Teacher = dto.Teacher;
+            kindergarten.CreatedAt = DateTime.Now;
+            kindergarten.UpdatedAt = DateTime.Now;
+
+            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.SaveChangesAsync();
+
+            return kindergarten;
+        }
+
+        public async Task<Kindergarten> DetailAsync(Guid id)
+        {
+            var result = await _context.Kindergartens
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
     }
 }
