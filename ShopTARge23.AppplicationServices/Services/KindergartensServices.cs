83014467c4f50018ae2341df642d175250e1a494
ccaups <ccaups@gmail.com>
2024-10-03 19:25:00 +0300

    KindergartensServices Detail added and fixed IkindergartensServices.

--- ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
+++ ShopTARge23.AppplicationServices/Services/KindergartensServices.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Dto;
+using ShopTARge23.Data;
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.ServiceInterface;
+
 
 namespace ShopTARge23.ApplicationServices.Services
 {
-    class KindergartensServices
+    public class KindergartensServices : IKindergartensServices
     {
+        private readonly ShopTARge23Context _context;
+
+        public KindergartensServices
+            (
+            ShopTARge23Context context
+            )
+        {
+            _context = context;
+        }
+        public async Task<Kindergarten> Create(KindergartenDto dto)
+        {
+            Kindergarten kindergarten = new Kindergarten();
+
+            kindergarten.Id = Guid.NewGuid();
+            kindergarten.GroupName = dto.GroupName;
+            kindergarten.ChildrenCount = dto.ChildrenCount;
+            kindergarten.KindergartenName = dto.KindergartenName;
+            kindergarten.Teacher = dto.Teacher;
+            kindergarten.CreatedAt = DateTime.Now;
+            kindergarten.UpdatedAt = DateTime.Now;
+
+            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.SaveChangesAsync();
+
+            return kindergarten;
+        }
+
+        public async Task<Kindergarten> DetailAsync(Guid id)
+        {
+            var result = await _context.Kindergartens
+                   .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
     }
-}
+
+ }
--- ShopTARge23.core/ServiceInterface/IKindergartensServices.cs
+++ ShopTARge23.core/ServiceInterface/IKindergartensServices.cs
@@ -1,18 +1,13 @@
 ﻿using ShopTARge23.Core.Domain;
 using ShopTARge23.Core.Dto;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShopTARge23.Core.ServiceInterface
 {
     public interface IKindergartensServices
     {
-        Task<Spaceship> DetailAsync(Guid id);
-        Task<Spaceship> Update(SpaceshipDto dto);
-        Task<Spaceship> Delete(Guid id);
-        Task<Spaceship> Create(SpaceshipDto dto);
+        Task<Kindergarten> DetailAsync(Guid id);
+        Task<Kindergarten> Update(KindergartenDto dto);
+        Task<Kindergarten> Delete(Guid Id);
+        Task<Kindergarten> Create(KindergartenDto dto);
     }
 }
