69d778e16010fdb43447004c3d145cf67aa70696
ccaups <ccaups@gmail.com>
2024-10-07 10:19:25 +0300

    Init RealEstate

---
+++ ShopTARge23.AppplicationServices/Services/RealEstateServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Dto;
+using ShopTARge23.Data;
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.ServiceInterface;
+
+namespace ShopTARge23.ApplicationServices.Services
+{
+    public class RealEstateServices : IRealEstateServices
+    {
+        private readonly ShopTARge23Context _context;
+        private readonly IFileServices _fileServices;
+
+        public RealEstateServices
+            (
+            ShopTARge23Context context,
+            IFileServices fileServices
+            )
+        {
+            _context = context;
+            _fileServices = fileServices;
+        }
+    }
+}
---
+++ ShopTARge23/Controllers/RealEstateController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge23.Data;
+using ShopTARge23.Core.ServiceInterface;
+
+namespace ShopTARge23.Controllers
+{
+    public class RealEstateController : Controller
+    {
+        private readonly ShopTARge23Context _context;
+        //private readonly IRealEstateServices _realEstateServices;
+
+        public RealEstateController
+            (
+            ShopTARge23Context context
+            //IRealEstateServices realEstateServices
+            )
+        {
+            _context = context;
+            //_realEstateServices = realEstateServices;
+
+        }
+        public IActionResult Index()
+        {
+            //var result = _context;
+            return View();
+        }
+    }
+}
